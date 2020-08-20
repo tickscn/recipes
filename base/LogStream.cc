d079146586e775472b6cd28e17d5aa2f7472386a
ticks <xiehuiwu1996@gmail.com>
2020-08-20 19:47:52 +0800

    为日志库准备的流, 自带一个缓冲区

---
+++ base/LogStream.cc
@@ -0,0 +1,228 @@
+#include "LogStream.h"
+#include <algorithm>
+#include <cassert>
+#include <cinttypes>
+#include <cstdint>
+#include <cstdio>
+#include <cstring>
+#include <limits>
+
+using namespace daina;
+using namespace daina::detail;
+
+namespace daina {
+namespace detail {
+    const char  // di
+        gits[]       = "9876543210123456789";
+    const char* Zero = digits + 9;
+    static_assert(sizeof(digits) == 20, "数字数目错误");
+    const char hexDigits[] = "0123456789ABCDEF";
+    static_assert(sizeof(hexDigits) == 17, "16进制数字数目错误");
+
+    /*
+     * 广义整型转字符串
+     */
+    template <typename T>
+    size_t convert(char buf[], T value)
+    {
+        T i     = value;
+        char* p = buf;
+        do
+        {
+            int lsd = static_cast<int>(i % 10);
+            i /= 10;
+            *p++ = Zero[lsd];
+        } while (i != 0);
+        if (value < 0)
+        {
+            *p++ = '-';
+        }
+        *p = 0;
+        std::reverse(buf, p);
+        return p - buf;
+    }
+    size_t convertHex(char buf[], uintptr_t value)
+    {
+        uintptr_t i = value;
+        char* p     = buf;
+        do
+        {
+            int lsd = static_cast<int>(i % 16);
+            i /= 16;
+            *p++ = hexDigits[lsd];
+        } while (i != 0);
+        *p = 0;
+        std::reverse(buf, p);
+        return p - buf;
+    }
+    template class LogBuffer<smallBufferSize>;
+    template class LogBuffer<largeBufferSize>;
+}  // namespace detail
+
+string formatAsSI(int64_t n)
+{
+    char buf[10];
+    if (n < 1000)
+        snprintf(buf, sizeof buf, "%" PRId64, n);
+    else if (n < 9995)
+        snprintf(buf, sizeof buf, "%.2fK", n / 1e3);
+    else if (n < 99950)
+        snprintf(buf, sizeof buf, "%.1fK", n / 1e3);
+    else if (n < 999500)
+        snprintf(buf, sizeof buf, "%" PRId64 "K", n / 1000);
+    else if (n < 9995000)
+        snprintf(buf, sizeof buf, "%.2fM", n / 1e6);
+    else if (n < 99950000)
+        snprintf(buf, sizeof buf, "%.1fM", n / 1e6);
+    else if (n < 999500000)
+        snprintf(buf, sizeof buf, "%" PRId64 "M", n / 1000000);
+    else if (n < 9995000000)
+        snprintf(buf, sizeof buf, "%.2fG", n / 1e9);
+    else if (n < 99950000000)
+        snprintf(buf, sizeof buf, "%.1fG", n / 1e9);
+    else if (n < 999500000000)
+        snprintf(buf, sizeof buf, "%" PRId64 "G", n / 1000000000);
+    else if (n < 9995000000000)
+        snprintf(buf, sizeof buf, "%.2fT", n / 1e12);
+    else if (n < 99950000000000)
+        snprintf(buf, sizeof buf, "%.1fT", n / 1e12);
+    else if (n < 999500000000000)
+        snprintf(buf, sizeof buf, "%" PRId64 "T", n / 1000000000000);
+    else if (n < 9995000000000000)
+        snprintf(buf, sizeof buf, "%.2fP", n / 1e15);
+    else if (n < 99950000000000000)
+        snprintf(buf, sizeof buf, "%.1fP", n / 1e15);
+    else if (n < 999500000000000000)
+        snprintf(buf, sizeof buf, "%" PRId64 "T", n / 1000000000000000);
+    else
+        snprintf(buf, sizeof buf, "%.2fE", n / 1e18);
+    return buf;
+}
+
+string formatAsICE(int64_t n)
+{
+    char buf[10];
+    const double Ki = 1024.0;
+    const double Mi = Ki * 1024.0;
+    const double Gi = Mi * 1024.0;
+    const double Ti = Gi * 1024.0;
+    const double Pi = Ti * 1024.0;
+    const double Ei = Ei * 1024.0;
+    if (n < Ki)
+        snprintf(buf, sizeof buf, "%" PRId64, n);
+    else if (n < 9.995 * Ki)
+        snprintf(buf, sizeof buf, "%.2f", n / Ki);
+    else if (n < 99.95 * Ki)
+        snprintf(buf, sizeof buf, "%.1f", n / Ki);
+    else if (n < 1023.5 * Ki)
+        snprintf(buf, sizeof buf, "%.0f", n / Ki);
+    else if (n < Mi * 9.995)
+        snprintf(buf, sizeof buf, "%.2fMi", n / Mi);
+    else if (n < Mi * 99.95)
+        snprintf(buf, sizeof buf, "%.1fMi", n / Mi);
+    else if (n < Mi * 1023.5)
+        snprintf(buf, sizeof buf, "%.0fMi", n / Mi);
+    else if (n < Gi * 9.995)
+        snprintf(buf, sizeof buf, "%.2fGi", n / Gi);
+    else if (n < Gi * 99.95)
+        snprintf(buf, sizeof buf, "%.1fGi", n / Gi);
+    else if (n < Gi * 1023.5)
+        snprintf(buf, sizeof buf, "%.0fGi", n / Gi);
+    else if (n < Ti * 9.995)
+        snprintf(buf, sizeof buf, "%.2fTi", n / Ti);
+    else if (n < Ti * 99.95)
+        snprintf(buf, sizeof buf, "%.1fTi", n / Ti);
+    else if (n < Ti * 1023.5)
+        snprintf(buf, sizeof buf, "%.0fTi", n / Ti);
+    else if (n < Pi * 9.995)
+        snprintf(buf, sizeof buf, "%.2fPi", n / Pi);
+    else if (n < Pi * 99.95)
+        snprintf(buf, sizeof buf, "%.1fPi", n / Pi);
+    else if (n < Pi * 1023.5)
+        snprintf(buf, sizeof buf, "%.0fPi", n / Pi);
+    else if (n < Ei * 9.995)
+        snprintf(buf, sizeof buf, "%.2fEi", n / Ei);
+    else
+        snprintf(buf, sizeof buf, "%.1fEi", n / Ei);
+
+    return buf;
+}
+}  // namespace daina
+template <size_t SIZE>
+void LogBuffer<SIZE>::cookieStart()
+{
+}
+template <size_t SIZE>
+void LogBuffer<SIZE>::cookieEnd()
+{
+}
+
+void LogStream::staticCheck()
+{
+    static_assert(maxNumbericSize - 10 > std::numeric_limits<double>::digits10, "maxNumbericSize is large enough");
+    static_assert(maxNumbericSize - 10 > std::numeric_limits<long double>::digits10, "maxNumbericSize is large enough");
+    static_assert(maxNumbericSize - 10 > std::numeric_limits<long>::digits10, "maxNumbericSize is large enough");
+    static_assert(maxNumbericSize - 10 > std::numeric_limits<long long>::digits10, "maxNumbericSize is large enough");
+}
+
+template <typename T>
+void LogStream::formatInteger(T v)
+{
+    if (_buffer.avail() >= maxNumbericSize)
+    {
+        size_t len = convert(_buffer.current(), v);
+        _buffer.mvForward(len);
+    }
+}
+
+LogStream& LogStream::operator<<(int v)
+{
+    formatInteger(v);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned v)
+{
+    formatInteger(v);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(long long v)
+{
+    formatInteger(v);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(unsigned long long v)
+{
+    formatInteger(v);
+    return *this;
+}
+
+LogStream& LogStream::operator<<(const void* p)
+{
+    uintptr_t v = reinterpret_cast<uintptr_t>(p);
+    if (_buffer.avail() >= maxNumbericSize)
+    {
+        char* buf  = _buffer.current();
+        buf[0]     = '0';
+        buf[1]     = 'x';
+        size_t len = convertHex(buf + 2, v);
+        _buffer.mvForward(len + 2);
+    }
+    return *this;
+}
+
+/*
+ * snprintf 使用的是Dragon4 算法
+ * 以后可以尝试使用 Grisu算法来替换
+ */
+LogStream& LogStream::operator<<(double v)
+{
+    if (_buffer.avail() > maxNumbericSize)
+    {
+        int len = snprintf(_buffer.current(), maxNumbericSize, "%.12g", v);
+        _buffer.mvForward(len);
+    }
+    return *this;
+}
---
+++ base/LogStream.h
@@ -0,0 +1,171 @@
+#ifndef DAINA_LOGSTREAM_H
+#define DAINA_LOGSTREAM_H
+#include "Types.h"
+#include "noncopyable.h"
+#include <cassert>
+#include <cstring>
+namespace daina {
+namespace detail {
+    const size_t smallBufferSize = 4000;
+    const size_t largeBufferSize = 4000 * 1000;
+    template <size_t SIZE>
+    class LogBuffer : noncopyable {
+    public:
+        LogBuffer() : _cur(_data)
+        {
+            setCookie(cookieStart);
+        }
+        ~LogBuffer()
+        {
+            setCookie(cookieEnd);
+        }
+
+        size_t avail() const
+        {
+            return end() - _cur;
+        }
+
+        void append(const char* buf, size_t len)
+        {
+            if (avail() > len)
+            {
+                memcpy(_cur, buf, len);
+                _cur += len;
+            }
+        }
+
+        const char* data() const
+        {
+            return _data;
+        }
+
+        size_t length() const
+        {
+            return _cur - _data;
+        }
+
+        char* current()
+        {
+            return _cur;
+        }
+
+        void mvForward(size_t offset)
+        {
+            _cur += offset;
+        }
+
+        void reset()
+        {
+            _cur = _data;
+        }
+
+        void bzero()
+        {
+            bZero(_data, sizeof(_data));
+        }
+
+        void setCookie(void (*cookie)())
+        {
+            _cookie = cookie;
+        }
+
+        string toString() const
+        {
+            return string(_data, length());
+        }
+
+    private:
+        const char* end() const
+        {
+            return _data + sizeof(_data);
+        }
+        static void cookieStart();
+        static void cookieEnd();
+
+    private:
+        void (*_cookie)();
+        char _data[SIZE];
+        char* _cur;
+    };
+}  // namespace detail
+class LogStream : noncopyable {
+    using self = LogStream;
+
+public:
+    using Buffer = detail::LogBuffer<detail::smallBufferSize>;
+    self& operator<<(bool v)
+    {
+        _buffer.append(v ? "1" : "0", 1);
+        return *this;
+    }
+    self& operator<<(int);
+    self& operator<<(unsigned);
+    self& operator<<(long long);
+    self& operator<<(unsigned long long);
+
+    self& operator<<(const void*);
+    self& operator<<(float f)
+    {
+        return *this << implicit_cast<double>(f);
+    }
+    self& operator<<(double);
+    self& operator<<(char c)
+    {
+        _buffer.append(&c, 1);
+        return *this;
+    }
+    self& operator<<(const char* str)
+    {
+        if (str)
+        {
+            _buffer.append(str, strlen(str));
+        }
+        else
+        {
+            _buffer.append("(NULL)", 6);
+        }
+        return *this;
+    }
+    self& operator<<(const unsigned char* str)
+    {
+        return *this << reinterpret_cast<const char*>(str);
+    }
+    self& operator<<(const string& str)
+    {
+        _buffer.append(str.c_str(), str.length());
+        return *this;
+    }
+    self& operator<<(const Buffer& b)
+    {
+        return *this << b.toString();
+    }
+
+    void append(const char* data, size_t len)
+    {
+        _buffer.append(data, len);
+    }
+
+    const Buffer& buffer() const
+    {
+        return _buffer;
+    }
+
+    void resetBuffer()
+    {
+        _buffer.reset();
+    }
+
+private:
+    void staticCheck();
+    template <typename T>
+    void formatInteger(T);
+
+private:
+    Buffer _buffer;
+    static const int maxNumbericSize = 32;
+};
+
+string formatAsSI(int64_t);
+string formatAsIEC(int64_t);
+}  // namespace daina
+#endif
